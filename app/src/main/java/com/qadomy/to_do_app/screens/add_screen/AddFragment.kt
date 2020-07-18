5055623ef88adb8bdde729cfda281308a98eca86
mac <ali.qadomy@hotmail.com>
2020-07-18 12:16:54 +0300

    Create menu for list, add, update fragments

--- app/src/main/java/com/qadomy/to_do_app/screens/add_screen/AddFragment.kt
+++ app/src/main/java/com/qadomy/to_do_app/screens/add_screen/AddFragment.kt
@@ -1,9 +1,7 @@
 package com.qadomy.to_do_app.screens.add_screen
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import com.qadomy.to_do_app.R
 
@@ -13,7 +11,13 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.add_fragment, container, false)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 
 }
--- app/src/main/java/com/qadomy/to_do_app/screens/list_screen/ListFragment.kt
+++ app/src/main/java/com/qadomy/to_do_app/screens/list_screen/ListFragment.kt
@@ -1,9 +1,7 @@
 package com.qadomy.to_do_app.screens.list_screen
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.qadomy.to_do_app.R
@@ -21,7 +19,18 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
+        view.list_layout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+
+        setHasOptionsMenu(true)
+
         return view
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
 }
